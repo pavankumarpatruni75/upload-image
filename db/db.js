144db8ea3856325637393a9bfc7611a956b57b3d
MMHYDIND\mmpp75 <pavankumar.patruni@magikminds.com>
2017-03-27 16:25:41 +0530

    commit

--- db/db.js
+++ db/db.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/images', function(err) {
+var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+
+mongoose.connect('mongodb://' + connection_string, function(err) {
     if(err) {
         console.log('err '+err);
     } else {
