e37ae0baff59869771bb3b525415440391e20125
MMHYDIND\mmpp75 <pavankumar.patruni@magikminds.com>
2017-03-27 18:18:49 +0530

    commit

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-app.listen(9090);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+  console.log("node server runnning")
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
 
 console.log('Server running on port : 9090');
